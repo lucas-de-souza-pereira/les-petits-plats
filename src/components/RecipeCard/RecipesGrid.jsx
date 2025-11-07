0fa8ff00454b7de1f24c87ac3505f3baa2c77318
Lucas D. <lucas.desouza.p@gmail.com>
2025-11-07 15:24:09 +0100

    add queryNotFound into recipesGrid and review CSS to 404 Page

--- src/components/RecipeCard/RecipesGrid.jsx
+++ src/components/RecipeCard/RecipesGrid.jsx
@@ -4,10 +4,15 @@ import RecipeCard from "@/components/RecipeCard/RecipeCard";
 import s from "./RecipesGrid.module.css";
 
 export default function RecipesGrid() {
-  const { filteredRecipes } = useTags()
+  const { filteredRecipes, selected ,liveQ } = useTags()
+
+  const queryNotFound = (liveQ && liveQ.trim()) || (selected.keywords.at(-1) ?? ""); 
   return (
+    <>
+    {filteredRecipes.length === 0 && (<p style={{textAlign:"center"}}>Aucune recette ne contient "{queryNotFound}", vous pouvez chercher « tarte aux pommes », « poisson », etc. </p>)}
     <div className={s.grid}>
       {filteredRecipes.map(r => <RecipeCard key={r.id} {...r} />)}
     </div>
+    </>
   )
 }
